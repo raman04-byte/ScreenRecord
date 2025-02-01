2beb58380e4fefe83ee87e88bdf24928dc2b0ab1
Raman Tank <ramantank04022002@gmail.com>
2025-02-01 13:02:18 +0530

    Initial Commit

---
+++ ScreenRecord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenRecord
+//
+//  Created by Raman Tank on 01/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenRecord/ScreenRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenRecordApp.swift
+//  ScreenRecord
+//
+//  Created by Raman Tank on 01/02/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
